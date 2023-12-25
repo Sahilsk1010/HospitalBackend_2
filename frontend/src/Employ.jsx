03dee019b926dcf4b25f9690ed6d48362d1c7855
Sahil S Kulkarni <ankursk999@gmail.com>
2023-12-25 15:43:12 +0530

    Added

---
+++ frontend/src/Employ.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import "./employ.css"; // Make sure to import your CSS file
+
+const Employ = () => {
+  return (
+    <div className="e-wrapper">
+      <div className="e-container1">
+        <h1>ಕರ್ನಾಟಕ</h1>
+        <img
+          className="h_log"
+          src="https://ahvs.karnataka.gov.in/frontend/opt1/images/center_logo/kar_main_logo.png"
+          alt="logo"
+        />
+        <h1>ಸರ್ಕಾರ</h1>
+      </div>
+      <div className="tp"></div>
+
+      <div class="e-container2">
+        <p>ಪಶು ಪಾಲನಾ ಮತ್ತು ಪಶು ಸಂಗೋಪನಾ ಇಲಾಖೆ</p>
+      </div>
+      {/* <!--
+    <div class="container21">
+        <p>
+            ಪಶು ಪಾಲನಾ ಮತ್ತು ಪಶು ಸಂಗೋಪನಾ ಇಲಾಖೆ
+        </p>
+    </div>
+--> */}
+
+      <div class="e-full">
+        <div class="e-container3 ">
+          <div class="e-hosp  1">
+            <div class="e-center e-cont1">ಪಶು ಆಸ್ಪತ್ರೆ</div>
+
+            <div class="e-center e-cont2">ರಾಯಬಾಗ</div>
+          </div>
+          {/* <div class="e-hosp 1">
+            <div class="e-center e-cont1">
+              <p>ಪಶು ಆಸ್ಪತ್ರೆ</p>
+            </div>
+            <div class="e-center e-cont2"></div>
+          </div> */}
+          <div class="e-hosp  1">
+            <div class="e-center e-cont1">ಪಶು ಆಸ್ಪತ್ರೆ</div>
+
+            <div class="e-center e-cont2">ಹಾರೂಗೇರಿ</div>
+          </div>
+          <div class="e-hosp  1">
+            <div class="e-center e-cont1">ಪಶು ಆಸ್ಪತ್ರೆ</div>
+
+            <div class="e-center e-cont2">ಕುಡಚಿ</div>
+          </div>
+        </div>
+        <div class="e-container3">
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2">ಅಳಗವಾಡಿ</div>
+          </div>
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2">ಬೆಂಡವಾಡ</div>
+          </div>
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2">ದಿಗ್ಗೆವಾಡಿ</div>
+          </div>
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2"> ಹಂದಿಗುಂದ</div>
+          </div>
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2">ಕೋಳಿಗುಡ್ಡ</div>
+          </div>
+        </div>
+        <div class="e-container3">
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2">ಮೊರಬ</div>
+          </div>
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2">ಮುಗಳಖೋಡ್</div>
+          </div>
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2">ನಸಲಾಪೂರ</div>
+          </div>
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2">ನಿಲಜಿ</div>
+          </div>
+          <div class="e-hosp1">
+            <div class="e-center e-cont1">ಪಶು ಚಕಿತ್ಸಾಲಯ</div>
+
+            <div class="e-center e-cont2">ಪರಮಾನಂದವಾಡಿ</div>
+          </div>
+        </div>
+        <div class="e-container3 ">
+          <div class="e-hosp 1">
+            <div class="e-center e-cont1">ಪ್ರಾಥಮಿಕ ಪಶು ಚಿಕತ್ಸಾ ಕೇಂದ್ರ</div>
+
+            <div class="e-center e-cont2">ಚಿಂಚಲಿ</div>
+          </div>
+          <div class="e-hosp  1">
+            <div class="e-center e-cont1">ಪ್ರಾಥಮಿಕ ಪಶು ಚಿಕತ್ಸಾ ಕೇಂದ್ರ</div>
+
+            <div class="e-center e-cont2">ಕಂಕಣವಾಡಿ</div>
+          </div>
+          <div class="e-hosp  1">
+            <div class="e-center e-cont1">ಪ್ರಾಥಮಿಕ ಪಶು ಚಿಕತ್ಸಾ ಕೇಂದ್ರ</div>
+
+            <div class="e-center e-cont2">ಮೇಖಳಿ</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Employ;
